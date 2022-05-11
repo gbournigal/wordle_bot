7fb176c58ab6ee73f838baab36f7d3c811c4277c
gbournigal <georgesbournigal@gmail.com>
2022-05-11 17:49:29 -0400

    Avances en la clase del juego y en la clase del bot

--- wordle_game_class.py
+++ wordle_game_class.py
@@ -28,40 +28,96 @@ valid_letters = {i: [0, 1, 2, 3, 4] for i in string.ascii_lowercase}
  
 class WordleGame():
     def __init__(self, valid_words, valid_solutions):
-        valid_letters = {i: [0, 1, 2, 3, 4] for i in string.ascii_lowercase}
         self.round = 0
         self.valid_words = valid_words
         self.valid_solutions = valid_solutions
-        self.valid_letters = valid_letters
         self.colors = {}
         self.tries = {}
         self.secret_word = None
+        self.status = None
         
     
     def define_secret(self, secret_word):
-        self.secret_word = secret_word
+        if secret_word in self.valid_solutions:
+            self.secret_word = secret_word
+        else:
+            print('Invalid word. Try again.')
         
     
-    def check_valid_word(self, guess):
-        pass
-        
     def try_word(self, guess):
-        if guess in self.valid_letters:
-            
-            for i in range(5):
-                if guess[i] == self.secret_word[i]:
-                    self.colors[i] = 'green'
-                    
-                    
-                elif guess[i] in self.secret_word:
-                    self.colors[i] = 'yellow'
-    
-                else:
-                    self.colors[i] = 'black'
-                    
+        if guess in self.valid_words:
             self.round += 1
+            self.tries[self.round] = guess
+            self.colors[self.round] = {}
+            
+            if self.secret_word == None:
+                for i in range(5):
+                    self.colors[self.round][i] = input(f'Write the colors of the letter: {i+1}')
+            
+            else:
+                for i in range(5):
+                    if guess[i] == self.secret_word[i]:
+                        self.colors[self.round][i] = 'green'
+                        
+                        
+                    elif guess[i] in self.secret_word:
+                        self.colors[self.round][i] = 'yellow'
+        
+                    else:
+                        self.colors[self.round][i] = 'black'
+            
+            self.check_status()
+                    
+        else:
+            print('Invalid word. Try again.')
+    
+    
+    def check_status(self):
+        if self.round == 6:
+            self.status = 'end'
+            
+        elif self.colors[self.round] == {0: 'green', 1: 'green', 2: 'green', 3: 'green', 4: 'green'}:
+            self.status = 'end'
         
         else:
-            print('Invalid word. Try again')
+            self.status = 'playing'
+            
+    
+class WordleBot():
+    def __init__(self):
+        self.valid_words = None
+        self.left_words = None
+        self.must_letters = []
+        self.green_letters = []
+        self.black_letters = []
+        self.wordle = None
         
     
+    def get_wordle_game(self, wordle):
+        self.wordle = wordle
+        self.valid_words = wordle.valid_words
+        self.left_words = wordle.valid_solutions
+        
+        
+    def solve_wordle(self):
+        pass
+    
+    
+    def update_left_words(self):
+        pass
+    
+    
+    def word_selector(self):
+        pass
+        
+    
+            
+        
+    
+wordle = WordleGame(valid_words, valid_solutions)
+
+wordle.define_secret('hello')
+
+wordle.try_word('hello')
+
+wordle.colors
